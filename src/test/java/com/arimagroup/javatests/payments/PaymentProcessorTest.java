88d9fd79915a132e3938fba4d49ab66492950716
Bryan Cipriano <ciprianot.bryan@gmail.com>
2021-10-21 00:44:33 -0500

    Using @Before

--- src/test/java/com/arimagroup/javatests/payments/PaymentProcessorTest.java
+++ src/test/java/com/arimagroup/javatests/payments/PaymentProcessorTest.java
@@ -1,18 +1,28 @@
 package com.arimagroup.javatests.payments;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 import static org.junit.Assert.*;
 
 public class PaymentProcessorTest {
+    private PaymentGateway paymentGateway;
+    private PaymentProcessor paymentProcessor;
+
+    @Before
+    public void setUp() throws Exception {
+        paymentGateway = Mockito.mock(PaymentGateway.class);
+        paymentProcessor = new PaymentProcessor(paymentGateway);
+    }
+
     @Test
     public void payment_is_correct() {
-        PaymentGateway paymentGateway = Mockito.mock(PaymentGateway.class);
+
         Mockito.when(paymentGateway.requestPayment(Mockito.any()))
                 .thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.OK));
 
-        PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
+
         assertTrue(paymentProcessor.makePayment(1000));
     }
 
